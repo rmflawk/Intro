ec2842d49d34083b415a966c60274ea299b3ce52
rmflawk <ant921111@naver.com>
2020-04-06 22:12:33 +0900

    1

---
+++ app/src/main/java/com/youngstudio/intro/MainActivity.java
@@ -0,0 +1,20 @@
+package com.youngstudio.intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
